87da6b0a63e481c53a471b9497934f31ac173669
shundroid <shundroidk@gmail.com>
2018-02-01 18:44:09 +0900

    added build.js

---
+++ build.js
@@ -0,0 +1,9 @@
+var exec = require('child_process').exec;
+
+exec('npm run build', function (error, stdout, stderr) {
+  console.log('stdout: ' + stdout);
+  console.log('stderr: ' + stderr);
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
+});
