c89e46fb7ee6c01f57497af7aabf7941942050e5
shundroid <shundroidk@gmail.com>
2018-02-02 22:27:12 +0900

    added soppy flag

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -15,7 +15,7 @@
       Project(:no-image="true" title="hexo-search-result" background="#0f83cd" color="#fff" link="https://github.com/shundroid/hexo-search-result")
         | You can make a search result page for #[a(href="https://github.com/PaicHyperionDev/hexo-generator-search") hexo-generator-search]
         | &nbsp;by using this.
-      Project(:no-image="true" title="sphero-editor" background="#009688" color="#fff" link="https://github.com/comozilla/sphero-editor")
+      Project(:no-image="true" title="sphero-editor" background="#009688" color="#fff" link="https://github.com/comozilla/sphero-editor" :soppy="true")
         | Developed with #[a(href="http://comozilla.org/") Comozilla Lab].#[br]
         | We held a workshop about #[a(href="https://www.sphero.com/") sphero] bowl.#[br]
         | This was used in the workshop to program sphero by javascript.
@@ -25,13 +25,13 @@
       Project(:no-image="true" title="sphero-websocket" background="#F44336" color="#fff" link="https://github.com/shundroid/sphero-websocket/tree/develop")
         | Control sphero via web-socket.
         | We have used this in the workshops which develop sphero-apps.
-      Project(:no-image="true" title="virtual-sphero" background="#3F51B5" color="#fff" link="https://github.com/comozilla/virtual-sphero/tree/develop")
+      Project(:no-image="true" title="virtual-sphero" background="#3F51B5" color="#fff" link="https://github.com/comozilla/virtual-sphero/tree/develop" :soppy="true")
         | We can simulate spheros when we don't have them.
         | It was very useful when we developed.
       Project(:no-image="true" title="themusic" background="#e0e0e0" color="#212121" link="https://github.com/chikoski/themusic")
         | A music player on Firefox OS.
         | Developed with #[a(href="https://github.com/chikoski") chikoski], a student who was during work visit.
-      Project(:no-image="true" title="threejs-kart" background="#43A047" color="#FFD180" link="https://github.com/shundroid/threejs-kart")
+      Project(:no-image="true" title="threejs-kart" background="#43A047" color="#FFD180" link="https://github.com/shundroid/threejs-kart" :soppy="true")
         | Developed in the workshop about three.js.
         | This is what I used Git for the first time, so the commit message is very strange.
 </template>
