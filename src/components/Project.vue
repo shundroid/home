16770cd176a460a3f6ec46aa46c25c73f74e9574
shundroid <shundroidk@gmail.com>
2021-05-03 13:19:44 +0900

    added projects

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -2,7 +2,8 @@
 article(:style="style")
   img(:src="thumbnail")
   h3
-    a(:href="link" target="_blank") {{ title }}
+    a(:href="link" target="_blank" v-if="link") {{ title }}
+    span(v-else) {{ title }}
   .slot-container
     slot
 </template>
@@ -13,7 +14,7 @@ export default {
     title: String,
     background: String,
     color: String,
-    link: String,
+    link: { type: String, required: false },
     thumbnail: { type: String, default: 'https://lh3.googleusercontent.com/D38C-EsaUIjY3T5u65KyXBAHVFty8oZT44v17uUBWezOGyUHxbeW0ZNhw32qrF7gZgu3tT1iaiWXghDFFzRjiohpA-QdYXKG1InPpxmjTIavzPRxUM-JWjpYgk3xo1h1aR8fORt92Fnm39Slg1JwgrSCTHtYCnytEVKUc6rvnDWNctpTxhl1nRz3QD4yhoHMV7auhMFQSZZED9uQ9H2mUMuXP6POzaaASlU3Q5g-9lWv1s2zyS_t37OxYEjvjTz-ex1_9BQWndHaTyHf54NaSrvjza0ZgTP6Zpg3AX9v8T6f5i9uwBAeOXIQPNOeV-2e49255ZGnSMfkF_9xnDBDCbCcYCeoySpX1nDDR8z0vu3fhiwfJfZOOYMR_BJwkffCfVgiPmeyHZbERM6HoL7mfcO4Qju2NFOyK8-y4RPDeHi0BK3w-bw91rrA3tJQFwGz0vlHaFLD_w4hqnpy-EJ8TwqzOWDni9aJbEE-zG_-womnQSwhQvrvJ_MIwz1mCWhASfQVyYBOPRpJR_Us9GQUq4kbSTjwm49vpeVvLVikEz64DdCTOOhTca-bqSgAJervtyCW_v9UoaV09sCpuw6O_03gtrh658Vysrvz_9JE3gvKuAirE2r7cGcYv2STxGcPil8L7zyDnHioa_ZpswDh3yNcWTlCATKGCeempVKK_fgeOOcfPyEJHJbQ49WXNx2lYAfv6EL1ctdA2UB8ikI6V-72=s256-no?authuser=0' }
   },
   computed: {
@@ -41,14 +42,17 @@ article {
 a {
   color: inherit;
 }
-h3 a {
-  text-decoration: none;
-}
 img {
   width: 100px;
 }
 .slot-container {
   flex: 1;
   overflow-y: auto;
+}
+</style>
+
+<style>
+.slot-container a {
+  color: inherit;
 }
 </style>
--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -19,7 +19,20 @@ main
     Project(title="ガロア理論ジェネレーター Galois-Gen" background="#2196f3" color="white" link="https://dotkai.netlify.app")
       | 高校の先生が出版した本の表紙を自由にデザインできるサイトです。#[br]
       | A website where we can design the front cover of the book our teacher published.
+  h3 Systems for school activities
+  | 私が開発した学校活動向けのシステムです。 Systems I have developed.#[br]#[br]
+  section
+    Project(title="現社ディベートシステム Debate System" background="#F44336" color="white" link="https://drive.google.com/file/d/1kspJnUJ1ZT0JAOym4qft5Y9wBOBMbvck/view?usp=sharing" thumbnail="https://lh3.googleusercontent.com/pw/ACtC-3ccZnc9aCiWeCCBxrdMgt1_Y6rZ-KjgMuZgZ4gBM3kahI7plbzpwLGJhSzxw7vbrJYhBA-iVu_tyYAUtjGdukmA6x_TazZyhTKB4UelStmTQDvhcs6tYLtHSdJpsVMHtQRqueIlgtqccoYdrXyLi6jnVA=s955-no?authuser=0")
+      | 高校の授業『現代社会』でのディベートで、投票を集計し、スプラトゥーンのように勝敗を表示するシステムです。#[br]
+      | A debate system for a high school subject "Modern Society". This collects votes and shows the result like the game Splatoon.
+    Project(title="サイコの予約 Psycho-Reservation" background="black" color="#F53F74" link="https://drive.google.com/file/d/1_1KWru3puUQZdxNLHCjjIpMtyk6XD4n9/view?usp=sharing" thumbnail="https://lh3.googleusercontent.com/pw/ACtC-3c6ZyporU1OcJd8sO3WE-krK0I2buXiSQaOwh7ScBa2owqowweywbMLBlIuq0bBHk0vuLGobIu0e8O7-VifYyPaXZDMmCoL_Kr2Ofb1WKDTaZd3zH7qs7bLxlA5L4zKAZSbL0XZQ-AiA2Qq2Dum8fy2Xw=s400-no?authuser=0")
+      | 文化祭での劇『サイコパス』の公演予約をLINEで行えるようにするシステムです。技術解説は#[a(href="https://suken.netlify.app/e2w/") こちら]。#[br]
+      | A system for a drama "Psychopath" in school festival. This enables us to reserve seats from LINE. #[a(href="https://suken.netlify.app/e2w/") Here] is technological description.
+    Project(title="記念祭備品管理 Kinensai Tool-Management" background="#eee" color="black" thumbnail="https://lh3.googleusercontent.com/2Mzs3vpAuz8EcmJayyORAOunWiKGXPK-YAQ31xVOq-wqRdV3_b8URIUvp4i1uvxYVtDICySFO1QnOf2a61M0adXfwp1VufRa9C_hRAU5T8g1W706xlez-n2ZYIyYa_BInCs_geuVRlKMsroHuXjRpjhd2rUtPptfjgmBmLHBU6Rfmkw_vq2mKpQ9TOZ8h8-mNGaAEB0KUjw1mmZZIPQXJuuFpqM51ne-j_fkwHsIJ9RnbF_PRbzwPoSPV4P6a_AQv59uOTJJtl9MANU5j67hHj_2_EI3_EGck3Q5XmLPcTQXZP9bjLb3IwL4bFtSuL6IKxfHrQet_aSbah4aP6-3_jxG0aIeucpiE2P4FZ9vH2CvF1b0JlqmivDAj2kJlLDiTjL344dG6MuzXmAxmBiRA9oituHRPDDQZH_M3-ASb4Z-1gfVZNJGC3vHdco8gmn_7oKp43qcp5pEPmiFeA3AQloCgONCTFXC1g8AFEGjvSDQCYt3UlKcSfpYcO5sq9wODG_bF3nIa_OkEQHCW5hTtYFsTAnn_9Qkmeb4dRfmK2Dr3vtb5i-O7PCQnidtbT7GOVATUXYqtXzoTRGkImgJX_vzvq5Zs3y4nLglM0OT6PIGSofLXQBZDGiz_IWGN247MxhAgasjBoGSJMlmyws6aPq7W6FyHbI_YY6Di1CwWOoyCJ0Lul6Y7jQt5cGFdmxkHvS6T1O_2V78GEF2kwgnccr9=s105-no?authuser=0")
+      | 文化祭で使う備品の貸出状況などを管理するシステムです。FirebaseのAuthとFirestoreを利用しています。#[br]
+      | A system for managing tools used for school festivals developed with Firebase's Auth and Firestore.
   h3 Systems for workshops
+  | 私が開発したワークショップ向けのシステムです。 Systems I have developed.#[br]#[br]
   section
     Project(title="animium.js" background="#FF9800" color="#fff" link="https://github.com/dengaku-club/animium.js" thumbnail="https://lh3.googleusercontent.com/0SN13-BoBtpYbpgdO2sv46izmN0TLM2jQCi8_NXwZtTkPJMFoZn84HwSxKzfTj_7QTNkjycmrdXATY3TDUOzFpaNLQbHIHll5dUoY_KohA5gINxKdNcdzmVNgqGD_nnKj2CEWFcU3DsBZ2u88_MqgACqJ_DJAs47yie7e2RL-PVlbCanPCdbmIjlPf2-w4fYvqMGu2qnizLPhoxSN8irjKXAzjOddLSQ6sASuiU7TT8wpO4CedMCN_cm5I-XDXOp2NDImYkp1okAtL5g8yMQWkyaI75YqyrvqKwyrVvVWGPBXKoCXpwvnci3wSBqtcEOIYCOMdDZfgm5emSUAr7MeR8q0drfd3ZjDikkPrS4Fqf51NFWsgn3LFnYb7dkVOltrEUAjfLo_vIXgcAsz15FICUu8tQAqiti7kyKeo0EIa3_twAC1KCTC2WPqU5KoCP5wTwIAyAksI-hoa_9FjQ0V11TzKcTTM8O8tyK2vrV8f6dyfNDtx7k2kwZChnWNaSC4JzKobvhmag149tyowBaVA4fXjC1CxKALtbiSdMDspJsl1vYy7iNV5Sf-ABTYrPwEDfQVElYEIRcCJPvFX4Qs-DUmRoFBCRqdACeO4PWk3JWxq5UrscI7S0vmHbYsa4HlUgbQAuly2fPaa-O0nIXrioPVsE63TtfA6RCQOEDgE9AH5Nv5qwUvJdV9bChsNHTJLOrdbtYh4KcRp1POQKju1fH=s256-no?authuser=0")
       | #[a(href="https://dengaku.club/") 電楽倶楽部]の方々と開発しました。アニメーションの仕組みを教える#[a(href="https://dengaku.club/workshops/archives/20170826-cganimation/") ワークショップ]で使いました。#[br]
@@ -30,10 +43,20 @@ main
       | Spheroの動作をプログラムするエディタやおにごっこルールのシステムを提供しています。
       | Developed in Comozilla Lab. In a #[a(href="https://comozilla.tumblr.com/post/149152863530/%E3%81%93%E3%82%93%E3%81%AB%E3%81%A1%E3%81%AF-8%E6%9C%882%E6%97%A5%E3%81%AB%E4%BB%A3%E5%AE%98%E5%B1%B1%E3%81%AB%E3%81%82%E3%82%8B-sodacco-%E3%81%AB%E3%81%A6-sphero") workshop]
       | in which children play tag using robots named Sphero, this provides an editor for programming Sphero and system for game management.
-    Project(title="sphero-editor" background="#009688" color="#fff" link="https://github.com/comozilla/sphero-editor" :soppy="true")
-      | コモジラ研究所にて開発しました。SpheroというロボットでボーリングをするワークショップでSpheroをプログラムするために使いました。
-      | Developed in Comozilla Lab. We used this to program Sphero in a workshop in which we bowl by using Sphero balls.
+    Project(title="sphero-editor" background="#009688" color="#fff" link="https://github.com/comozilla/sphero-editor")
+      | コモジラ研究所にて開発しました。Spheroというロボットでボーリングをする#[a(href="https://comozilla.tumblr.com/post/160548893090/sphero%E3%81%A7%E3%83%9C%E3%83%BC%E3%83%AA%E3%83%B3%E3%82%B0%E3%81%AE%E3%83%AF%E3%83%BC%E3%82%AF%E3%82%B7%E3%83%A7%E3%83%83%E3%83%97%E3%82%92%E5%87%BA%E5%B1%95%E3%81%97%E3%81%BE%E3%81%97%E3%81%9F") ワークショップ]でSpheroをプログラムするために使いました。
+      | Developed in Comozilla Lab. We used this to program Sphero in a #[a(href="https://comozilla.tumblr.com/post/160548893090/sphero%E3%81%A7%E3%83%9C%E3%83%BC%E3%83%AA%E3%83%B3%E3%82%B0%E3%81%AE%E3%83%AF%E3%83%BC%E3%82%AF%E3%82%B7%E3%83%A7%E3%83%83%E3%83%97%E3%82%92%E5%87%BA%E5%B1%95%E3%81%97%E3%81%BE%E3%81%97%E3%81%9F") workshop] in which we bowl by using Sphero balls.
+  h3 Hobbies
+  | 私が開発した色々なものです。 Something I have developed.#[br]#[br]
+  section
+    Project(title="Decolony (ref-viz)" background="#e0e0e0" color="#212121" link="https://suken.netlify.app/decolony/" thumbnail="https://lh3.googleusercontent.com/pw/ACtC-3cZ9OHaNiXKzUsPj_2Nz4CofGlaU24lJtOZ_VAVfW02V2tP3d1zIgLP1nwVCwBwi19CVrgEVYP9waSJG3vsFgEC62uEGJfOFUoIL4jkkMRH6L6sS1avqRqrAuxW7OkaLKoSG2h9_T0REhmZIYZSKY6b_Q=s380-no?authuser=0")
+      | JavaScriptで書かれたアプリの参照関係を可視化します。アーキテクチャがどれだけ綺麗かが分かります。#[br]
+      | This visualizes references in apps written in JavaScript. You can see whether the architecture is tidy or not.
+    Project(title="themusic" background="#e0e0e0" color="#212121" link="https://github.com/chikoski/themusic")
+      | A music player for Firefox OS.
+      | Developed with #[a(href="https://github.com/chikoski") chikoski] and a student who was during work visit.
   h3 Libraries
+  | 私が開発したライブラリです。 Libraries I have developed.#[br]#[br]
   section
     Project(title="hexo-search-result" background="#0f83cd" color="#fff" link="https://github.com/shundroid/hexo-search-result")
       | You can make a search result page for #[a(href="https://github.com/PaicHyperionDev/hexo-generator-search") hexo-generator-search]
@@ -44,19 +67,23 @@ main
     Project(title="sphero-websocket" background="#F44336" color="#fff" link="https://github.com/shundroid/sphero-websocket/tree/develop")
       | You can control Sphero via web-socket.
       | We have used this in several workshops.
-    Project(title="virtual-sphero" background="#3F51B5" color="#fff" link="https://github.com/comozilla/virtual-sphero/tree/develop" :soppy="true")
+    Project(title="virtual-sphero" background="#3F51B5" color="#fff" link="https://github.com/comozilla/virtual-sphero/tree/develop")
       | We can use this for simulating Sphero even when we don't have them.
       | It was very useful when we developed apps using Sphero.
   h3 Games
+  | 私が開発したゲームです。 Games I have developed.#[br]#[br]
   section
-    Project(title="threejs-kart" background="#43A047" color="#FFD180" link="https://github.com/shundroid/threejs-kart" :soppy="true")
+    Project(title="公式をその場で作るゲーム" background="black" color="white" link="https://drive.google.com/file/d/1OHZfpPPFtNBTOKtijZXIAmvtAKoqatDy/view?usp=sharing" thumbnail="https://lh3.googleusercontent.com/pw/ACtC-3fq6uErg-XsW0nd8bvV5H2TAAGcRM0CWVJmzPsKFESpGZro4fCbM8MIuMvS-ZeDYvRWCw2Bkhc1NG6KzFFcxeKU6bAXk-MWvoLIFBHNUiCU6YQ4fikgGanLHsAdLoO1KuRVoZM6ycrQp3t_IVPB0fzJWQ=w392-h391-no?authuser=0")
+      | 顧問の名言「公式はその場で作る物です」をもとに、様々な公式をミニゲームを通じてその場で導いていきます。
+    Project(title="threejs-kart" background="#43A047" color="#FFD180" link="https://github.com/shundroid/threejs-kart")
       | Developed in a workshop on three.js.
       | This is my first project I used Git, so commit messages are strange. Don't mind!
-  h3 Others
-  section
-    Project(title="themusic" background="#e0e0e0" color="#212121" link="https://github.com/chikoski/themusic")
-      | A music player for Firefox OS.
-      | Developed with #[a(href="https://github.com/chikoski") chikoski] and a student who was during work visit.
+    Project(title="UFO-Escape" background="#1565C0" color="white" link="http://polaris-bear.lolipop.jp/game/" thumbnail="https://lh3.googleusercontent.com/pw/ACtC-3cPLzaakEwS_k3AAmPKOGmBf4Qo4xIguyXIUd-kQMtpIJb3wSmAueuqYTvtPd6fV0cTNf75MUDuR1TcV4OTwQwEjnoy8zCVKke_aYCR4XNgTXrB8GpoF7CTwxV579fnhO4bx7UrnyNf2W6zeGgh8rsGDg=s256-no?authuser=0")
+      | 小学生のころにenchant.jsを用いて作ったゲームです。受賞歴があります。コンテストページはこちら(#[a(href="https://mashupawards.tumblr.com/post/102370840146/finalstage-15-ma10") 1],&nbsp;#[a(href="http://blog.5jcup.org/2014/07/html5-japan-cup-2014_85.html") 2])。
+      | An enchant.js game I made when I was an elementary school student. I got prizes with this game. Here are contest pages (#[a(href="https://mashupawards.tumblr.com/post/102370840146/finalstage-15-ma10") 1],&nbsp;#[a(href="http://blog.5jcup.org/2014/07/html5-japan-cup-2014_85.html") 2]).
+    Project(title="ハルスター・アクション" background="#FFECB3" color="black" link="https://asterisks.netlify.app/2016/12/23/halstar/" thumbnail="https://lh3.googleusercontent.com/pw/ACtC-3feBObe9MR8jYDkozcwCT2Yfd2rz2stgRsIPxsRCvckqHGjQrP_5osyey1euWK9QMm1o8pdbwAxNqpuY7YuogyojzMrlAxJfKASDFC-G9OSRBXjDG2B04QRUQ2kDbFWUwZzSWBP6QKBQu2nw4LTbi9NSA=w525-h526-no?authuser=0")
+      | 小学生のころに、友達と一緒に作ったゲームです。Unityで作りました。
+      | A unity game I made when I was an elementary school student. Making a game with friends was very interesting.
 </template>
 
 <script>
